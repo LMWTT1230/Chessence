b3a6a6d8c77f275760a3a302eea419bd73e0e8d6
Xiuyuan Qiu <xiuyuangithub@gmail.com>
2023-10-27 17:00:08 -0700

    added login page

---
+++ chessence-frontend/src/pages/Login/LoginPage.js
@@ -0,0 +1,12 @@
+import { useState, useRef, useEffect } from "react";
+import LoginBox from "./LoginBox";
+export default function LoginPage() {
+  return (
+    <div id="loginPage">
+        <h1>chessence</h1>
+        <LoginBox/>
+    </div>
+  );
+}
+
+
