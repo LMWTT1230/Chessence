a24619f76fe12f37a0a181025d6ad38f28d54d12
Xiuyuan Qiu <xiuyuangithub@gmail.com>
2023-10-31 12:46:41 -0700

    add some documentation links

--- chessence-frontend/src/pages/Redirect/LoginRedirectPage.jsx
+++ chessence-frontend/src/pages/Redirect/LoginRedirectPage.jsx
@@ -1,7 +1,8 @@
 import { useIsAuthenticated,
     useMsal } from "@azure/msal-react";
 import { loginRequest } from '../../authConfig';
-
+//https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-browser/docs/acquire-token.md
+//https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-react/docs/getting-started.md#sign-a-user-in-using-the-login-apis-provided-by-azuremsal-browser
 export default function RedirectPage() {
     const { instance, accounts, inProgress } = useMsal();
     const isAuthenticated = useIsAuthenticated();
