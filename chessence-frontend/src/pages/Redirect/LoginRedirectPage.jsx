42f1712c2478ab8286ff936e48caab4fbc02bc2e
Xiuyuan Qiu <xiuyuangithub@gmail.com>
2023-10-31 12:24:59 -0700

    add display users' name and email

--- chessence-frontend/src/pages/Redirect/LoginRedirectPage.jsx
+++ chessence-frontend/src/pages/Redirect/LoginRedirectPage.jsx
@@ -1,11 +1,16 @@
-import { useIsAuthenticated } from "@azure/msal-react";
+import { useIsAuthenticated,
+    useMsal } from "@azure/msal-react";
 
 export default function RedirectPage() {
+    const { instance, accounts, inProgress } = useMsal();
     const isAuthenticated = useIsAuthenticated();
+    
     return (
         <>
             <p>{isAuthenticated ?
                 "Authenticated" : "Not Authenticated"}</p>
+            {accounts.map((user) => 
+                    <p>Hello, {user.name} ({user.username})!</p> )}
         </>
     )
 }
