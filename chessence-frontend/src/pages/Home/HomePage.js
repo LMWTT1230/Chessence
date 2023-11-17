a77e6ab43e6beb844553c2caa7c328e180ae36d0
Frank Assumma <fassumma@calpoly.edu>
2023-11-17 09:25:38 -0800

    Home Page Complete

--- chessence-frontend/src/pages/Home/HomePage.js
+++ chessence-frontend/src/pages/Home/HomePage.js
@@ -1,15 +1,29 @@
 import React from 'react';
+//import { useHistory } from 'react-router-dom';
 import './home.css';
 
 export default function HomePage() {
-  return (
-    <div className="container">
-      <h1>chessence</h1>
-      <div className="button-container">
-        <button className="button login-button">Login</button>
-        <p>Or</p>
-        <button className="button signup-button">Sign Up</button>
-      </div>
-    </div>
-  );
+    /*const history = useHistory();
+
+    const handleLogin = () => {
+        history.push('/login');
+    };
+    
+    const handleSignUp = () => {
+        history.push('/register');
+    };*/
+    return (
+        <div className="container">
+            <h1>chessence</h1>
+                <div className="button-container">
+                    <a href="/login">
+                        <button className="button login-button">Login</button>
+                    </a>
+                    <p>Or</p>
+                    <a href="/register">
+                        <button className="button signup-button">Sign Up</button>
+                    </a>
+                </div>
+        </div>
+    );
 };
