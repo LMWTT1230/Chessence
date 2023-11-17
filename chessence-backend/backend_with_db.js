8bc91fa9c9688c722de4d65f1b2a0d1fc3bddfed
Edson-05 <73969622+Edson-05@users.noreply.github.com>
2023-11-17 10:28:15 -0800

    added game model and implemented rest api

--- chessence-backend/backend_with_db.js
+++ chessence-backend/backend_with_db.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 
 import userServices from "./models/user-services.js";
+import gameServices from "./models/game-services.js";
 
 const app = express();
 const port = 8000;
@@ -45,6 +46,33 @@ app.put("/profile/:id", async (req, res) => {
         res.status(200).send(result);
     } else {
         res.status(400).end();
+    }
+});
+
+app.get("/history", async (req, res) => {
+    const id = req.query["id"];
+    try {
+        const result = await gameServices.getGames(id);
+        res.send({ games_list: result });
+    } catch (error) {
+        res.status(500).send("An error ocurred in the server.");
+    }
+});
+
+app.post("/results", async (req, res) => {
+    const game = req.body;
+    const savedGame = await gameServices.addGame(game);
+    if (savedGame) res.status(201).send(savedGame);
+    else res.status(500).end();
+});
+
+app.delete("history/:id", async (req, res) => {
+    const id = req.params["id"];
+    const result = await gameServices.deleteGame(id);
+    if (!result) {
+        res.status(404).send("Resource not found.");
+    } else {
+        res.status(204).end();
     }
 });
 
---
+++ chessence-backend/models/game-services.js
@@ -0,0 +1,70 @@
+import mongoose from "mongoose";
+import gameModel from "./game.js";
+
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose.set("debug", true);
+
+mongoose.connect(
+    "mongodb+srv://" +
+        process.env.MONGO_USER +
+        ":" +
+        process.env.MONGO_PWD +
+        "@" +
+        process.env.MONGO_CLUSTER +
+        "/" +
+        process.env.MONGO_DB +
+        "?retryWrites=true&w=majority",
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }
+);
+
+async function getGames(id) {
+    let result;
+    if (id) {
+        result = await findGameById(id);
+    } else {
+        result = await gameModel.find();
+    }
+    return result;
+}
+
+async function findGameById(id) {
+    try {
+        return await gameModel.findById(id);
+    } catch (error) {
+        console.log(error);
+        return undefined;
+    }
+}
+
+async function addGame(game) {
+    try {
+        const gameToAdd = new gameModel(game);
+        const savedGame = await gameToAdd.save();
+        return savedGame;
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
+}
+
+async function deleteGame(id) {
+    try {
+        await gameModel.findIdAndDelete(id);
+        return true;
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
+}
+
+export default {
+    getGames,
+    addGame,
+    deleteGame,
+};
