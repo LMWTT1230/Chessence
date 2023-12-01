554de588f0de8dd8daa578c914deed42a2f1b5e1
Will <willkang0131@gmail.com>
2023-12-01 11:23:20 -0800

    update index.js

--- chessence-frontend/src/index.js
+++ chessence-frontend/src/index.js
@@ -33,7 +33,7 @@ const root = ReactDOMClient.createRoot(container);
 export default function App() {
     return (
         <MsalProvider instance={msalInstance}>
-            <BrowserRouter basename="chessence-frontend">
+            <BrowserRouter basename="/chessence-frontend">
                 <Navbar />
                 <Routes>
                     <Route path="/" element={<HomePage />} />
