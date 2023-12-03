8b9d8bf459eccd78010da87824af5a88a87e9d25
Xiuyuan Qiu <xiuyuangithub@gmail.com>
2023-12-03 09:58:48 -0800

    move socket connection to game page

--- chessence-frontend/src/pages/ChessBoard/GamePage.js
+++ chessence-frontend/src/pages/ChessBoard/GamePage.js
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Board from "./ChessBoard.js";
 import { useLocation } from "react-router-dom";
+import { io } from "socket.io-client";
 
-export default function GamePage({socket}) {
+export default function GamePage() {
+    // connect to socket
+    useEffect(() => {
+        const socket = io("http://localhost:4000"); // CHANGE TO EMPTY IN DEPLOYMENT
+    }, []);
+
     const { state } = useLocation();
     const { time } = state;
     return <Board initTime={time} />;
