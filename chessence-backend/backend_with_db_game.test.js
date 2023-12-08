fbbc6f44f5bd49045d189c93e79d1d8925e3a3f4
Edson-05 <73969622+Edson-05@users.noreply.github.com>
2023-12-08 09:57:41 -0800

    Added Test Cases to User & GameModel 100% Coverage

--- chessence-backend/backend_with_db_game.test.js
+++ chessence-backend/backend_with_db_game.test.js
@@ -22,12 +22,40 @@ test("test getGames", async () => {
 
     const games = await gameServices.getGames();
 
-    //expect(games.length).toBe(2);
+    expect(games[0]["blackID"]).toBe(game1.blackID);
+    expect(games[1]["blackID"]).toBe(game2.blackID);
     await gameServices.deleteGame(addedGame1["_id"]);
     await gameServices.deleteGame(addedGame2["_id"]);
 });
 
-test("test getGamesById", async () => {
+test("test get one Game", async () => {
+    const game1 = {
+        blackID: "abcd",
+        whiteID: "1234",
+        gameHistory: "a1b2c3",
+        date: "2023-11-17T20:00:00Z",
+        winner: "abcd",
+    };
+
+    const game2 = {
+        blackID: "1234",
+        whiteID: "abcd",
+        gameHistory: "c3b2a1",
+        date: "2023-11-18T02:00:00Z",
+        winner: "1234",
+    };
+
+    const addedGame1 = await gameServices.addGame(game1);
+    const addedGame2 = await gameServices.addGame(game2);
+
+    const games = await gameServices.getGames(addedGame1["_id"]);
+
+    expect(games["blackID"]).toBe(game1.blackID);
+    await gameServices.deleteGame(addedGame1["_id"]);
+    await gameServices.deleteGame(addedGame2["_id"]);
+});
+
+test("test getGamesById exists", async () => {
     const game = {
         blackID: "abcd",
         whiteID: "1234",
@@ -40,6 +68,22 @@ test("test getGamesById", async () => {
 
     expect(addedGameByID).toBeTruthy;
     await gameServices.deleteGame(addedGame["_id"]);
+});
+
+test("test getGamesById doesn't exists", async () => {
+    const game = {
+        blackID: "abcd",
+        whiteID: "1234",
+        gameHistory: "a1b2c3",
+        date: "2023-11-17T20:00:00Z",
+        winner: "abcd",
+    };
+    const addedGame = await gameServices.addGame(game);
+    const gameId = addedGame["_id"];
+    await gameServices.deleteGame(gameId);
+    const addedGameByID = await gameServices.getGames(gameId);
+
+    expect(addedGameByID).toBeFalsy;
 });
 
 test("test addGame", async () => {
@@ -57,6 +101,26 @@ test("test addGame", async () => {
     expect(addedGame.blackID).toBe("abcd");
     expect(addedGame.whiteID).toBe("1234");
     await gameServices.deleteGame(addedGame["_id"]);
+});
+
+test("test add same Game", async () => {
+    const game = {
+        blackID: "abcd",
+        whiteID: "1234",
+        gameHistory: "a1b2c3",
+        date: "2023-11-17T20:00:00Z",
+        winner: "abcd",
+    };
+
+    const addedGame1 = await gameServices.addGame(game);
+    const addedGame2 = await gameServices.addGame(game);
+
+    const games = await gameServices.getGames();
+
+    expect(games[0]["blackID"]).toBe(game.blackID);
+    expect(games[1]["blackID"]).toBe(game.blackID);
+    await gameServices.deleteGame(addedGame1["_id"]);
+    await gameServices.deleteGame(addedGame2["_id"]);
 });
 
 test("test deleteGame", async () => {
